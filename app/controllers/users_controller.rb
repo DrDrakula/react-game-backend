6817980b719529aaec90e5884720e2c803bb5172
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-05 17:38:01 -0500

    completed lul

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
 
   def index
-    
+
     @users = User.all
     render json: @users
   end
 
+  def create
+    @user = User.new(username: params[:username], password: params[:password])
+    if @user.valid?
+      @user.save
+      token = issue_token({ 'user_id': @user.id })
+      render json: {'token': token }
+    else
+      render json: {error: 'User already Exists'}
+    end
+
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "joseph", password: "1")
+User.create(username: "nemanja", password: "2")
+
+Score.create(points: 69, user_id: 1)
+Score.create(points: 1000, user_id: 1)
+Score.create(points: 133, user_id: 1)
+Score.create(points: 0, user_id: 2)
+Score.create(points: 344, user_id: 2)
+Score.create(points: 5234, user_id: 2)
