fd368c16490ad6415238392149a18eef3ede6416
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-06 11:15:33 -0500

    added new score and leaderbord routes

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,6 +2,19 @@ class ScoresController < ApplicationController
 
   def index
     scores = Score.all
-    render json: scores
+    scores_with_users = scores.map do |score|
+      {username: score.user.username, points: score.points, id: score.id}
+    end
+
+    scores_with_users = scores_with_users.sort_by do |sth|
+      sth[:points]
+    end.reverse()
+
+    render json: scores_with_users
+  end
+
+  def create
+    user_id = token_user_id
+    Score.create(user_id: user_id, points: params[:score])
   end
 end
